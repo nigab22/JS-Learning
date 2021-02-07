3c3e93b27ecf8ec6cb2af6e393db02c2bc7c6543
nigab22 <76473196+nigab22@users.noreply.github.com>
2021-02-07 18:40:28 -0500

    Update Javascript Promises.js

--- Javascript Promises.js
+++ Javascript Promises.js
@@ -49,39 +49,3 @@ cup1.brewCoffee();
 let cup2 = new CoffeeMachine('espresso');
 cup2.brewCoffee();
 
-/**
- * const heatWater = new Promise((resolve, reject) => {
-  let heatWater = true;
-  if (heatWater) {
-    setTimeout(() => resolve("water heated"), 3000);
-  } else {
-    reject("Error! Water could not be heated.");
-  }
-});
-//heatWater.then(result => console.log(result), error => console.log(error));
-
-const grindCoffee = new Promise((resolve, reject) => {
-  let grindCoffee = true;
-  if (grindCoffee) {
-    setTimeout(resolve("coffee grounded"), 4000);
-  } else {
-    reject("Error! Coffee was not grounded");
-  }
-});
-
-//grindCoffee.then(result => console.log(result), error => console.log(error));
-
-
-Promise.all([heatWater, grindCoffee]).then(
-  result => {
-    console.log(result[0]);
-    console.log(result[1]);
-    console.log("Enjoy your coffee");
-  },
-  error => {
-    console.log(error);
-    console.log("No coffee for you");
-  }
-);
-
- */
